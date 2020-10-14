a772fc9e58deae48c187fe7c048cf6902b9d80cb
Luke Shay <38476395+LukeShay@users.noreply.github.com>
2020-10-14 03:10:36 +0000

    fixed linting errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/tailwind.css';
 import React from 'react';
 import { AppProps } from 'next/app';
 import NavBar from '../components/NavBar';
-import { projects } from '../lib/projects';
 
 function App({ Component, pageProps }: AppProps) {
   return (
