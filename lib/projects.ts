17d8a07a3841ad417d445274219bb893e9af34d0
Luke Shay <shay.luke17@gmail.com>
2021-01-09 09:45:38 -0600

    fixed some tings

--- lib/projects.ts
+++ lib/projects.ts
@@ -4,6 +4,5 @@ export default [
   { path: `${Paths.PROJECTS}/personal-site`, title: 'Personal Site' },
   { path: `${Paths.PROJECTS}/route-rating`, title: 'Route Rating' },
   { path: `${Paths.PROJECTS}/lapi`, title: 'Lapi' },
-  { path: `${Paths.PROJECTS}/canvas-v2`, title: 'Canvas V2' },
   { path: `${Paths.PROJECTS}/tower-defense`, title: 'Tower Defense' },
 ];
